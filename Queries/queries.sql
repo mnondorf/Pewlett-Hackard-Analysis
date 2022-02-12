b491027c99a7a7c8141b79358d7b6e84b9accb70
Melissa <melisochka926@gmail.com>
2022-02-11 20:54:36 -0600

    Upload for Assignment Submission

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,11 +1,29 @@
-CREATE TABLE titles (     
+-- drop table departments cascade;
+-- drop table employees cascade;
+-- drop table dept_manager cascade;
+-- drop table dept_emp cascade;
+-- drop table titles cascade;
+-- drop table salaries cascade;
+-- drop table employee_info cascade;
+-- drop table retirement_info cascade;
+
+
+-- Creating tables for PH-EmployeeDB
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+CREATE TABLE employees (     
 	emp_no INT NOT NULL,
-     title VARCHAR NOT NULL,
-     from_date date not null,
-    to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-     PRIMARY KEY (emp_no, title, from_date)
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
@@ -18,7 +36,14 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
-drop table dept_emp
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
@@ -31,7 +56,15 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
-select * from dept_emp
+CREATE TABLE titles (     
+	emp_no INT NOT NULL,
+     title VARCHAR NOT NULL,
+     from_date date not null,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
+);
 
 -- Retirement eligibility
 SELECT first_name, last_name
@@ -50,3 +83,166 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+select * from retirement_info;
+
+drop table employee_info cascade;
+
+-- Create new table for retiring employees with salary
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+INTO employee_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- Check the table
+SELECT * FROM employee_info;
+
+--DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+		select * from manager_info;
+		
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+select * from dept_info;
+
+-- sales team request
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_only
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales');
+
+select * from sales_only;
+
+-- Sales and Development teams request
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_n_dev
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales') or (d.dept_name = 'Development');
+
+select * from sales_n_dev;
+
+drop table retirement_titles cascade;
+
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
+-- Use Dictinct with Orderby to remove duplicate rows
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE rt.to_date = ('9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Use Distinct with Order By to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE rt.to_date = ('9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT COUNT(title) as ct, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY ct DESC;
+
+
+SELECT DISTINCT ON (emp_no) 
+e.emp_no, e.first_name, e.last_name, e.birth_date, 
+de.from_date, de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = ('9999-01-01')) AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Pewlett-Hackard-Analysis
+
+## Overview
+
+The two purposes of this project are to determine the employees eligible to retire soon and those eligible to participate in a mentorship program at the Pewlett Hackard company. The goal is to help Pewlett Hackard leadership prepare for a potential "silver tsunami" of many of their older employees retiring close together. 
+
+## Results
+
+The two deliverables that Pewlett Hackard requested were:
+
+1. Number of Retiring Employees by Title
+
+![Retiring Title Count](Data/Retirement_titles_ordered_by_title.png)
+
+
+2. Employees Eligible for the Mentorship Program
+
+![Mentorship Eligibility](Data/mentorship_eligibility_count_by_title.png)
+
+There are four main takeaways from this analysis:
+
+- There is very likely an error in this analysis. When we ran queries to establish the number of current employees, the result we returned was 33, 118 (see below) and yet the "Number of Retiring Employees by Title" totals to 72, 458, despite attempting to use the "SELECT DISTINCT ON" query to avoid this. 
+
+### Current Employee Count
+![Current Employee Count](Data/Current_emp.png)
+
+### "Retiring Titles" Query
+![Retiring Titles Query](Data/Queries_for_retiring_titles.png)
+
+- If we assume that our retults are indeed correct, there is a huge shortfall between the number of employees eligible to retire as a whole (72,458) and the number of such employees eligible for the mentorship program (1,549).
+
+- When looked at by department, some departments are in more trouble than others. Both the Senior Engineer and Senior Staff groups have only approximately 1% of their retiring employees eligible for the program, whereas the Assistant Engineer group has 6% eligibility.
+ 
+- The Manager group has no employees eligible for the mentorship program, despite having two employees eligible to retire soon.
+
+
+## Summary
+
+Instructions: 
+
+[Provide high-level responses to the following questions, then provide two additional queries or tables that may provide more insight into the upcoming "silver tsunami."
+
+    How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+
+    Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?]
+
+- I  don't feel that I can answer this at the moment because I believe that my analysis is incorrect.
+
+
